837430e8faa26f4205b0d911484c1eb1122ea5fa
TOneal419 <tyler.michael.oneal@gmail.com>
2022-01-31 14:22:04 -0600

    Refactored integration tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,23 +2,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
-
-  scenario 'valid inputs title and author' do
-    visit new_book_path
-    fill_in 'Title', with: 'harry potter'
-    ill_in 'Author', with: 'jk'
-    click_on 'Create Book'
-    visit books_path
-
-    expect(page).to have_content('harry potter')
-    expect(page).to have_content('jk')
-  end
   
   scenario 'valid inputs title author and price' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
-    fill_in 'Price', with: '35'
     fill_in 'Author', with: 'jk'
+    fill_in 'Price', with: '35'
     click_on 'Create Book'
     visit books_path
 
